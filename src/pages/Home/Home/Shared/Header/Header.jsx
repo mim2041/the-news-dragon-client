fb215a6234f6ab4154d22ef45ec002865cad36d4
mim2041 <suraiyamim338@gmail.com>
2023-04-21 22:14:51 +0600

    updating right side of the newspaper

--- src/pages/Home/Home/Shared/Header/Header.jsx
+++ src/pages/Home/Home/Shared/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../../../../../src/assets/logo.png';
 import moment from 'moment';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import Marquee from "react-marquee-slider";
 
 const Header = () => {
@@ -14,10 +14,26 @@ const Header = () => {
             </div>
             <div>
                 <Button variant="danger">Danger</Button>
-                <Marquee>
-                    I can be a react component.
-                </Marquee>
+                <p>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</p>
             </div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Career</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+            <Button variant="secondary">login</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </Container>
     );
 };
--- src/pages/Home/Home/Shared/Rightnav/RightNav.jsx
+++ src/pages/Home/Home/Shared/Rightnav/RightNav.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGithub, FaGoogle, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import QZone from '../../QZone/QZone';
+import bg from '../../../../../assets/bg.png';
+
+
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right nav</h2>
+            <h4>Login With</h4>
+            <Button className='mb-2' variant="outline-primary"><FaGoogle /> Login with Google</Button>
+            <Button variant="outline-secondary"><FaGithub /> Login with Github</Button>
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook/> facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter/> twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram/> instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
